0563fafc0bf77dfcf7902472da9910bf721b7776
Ahsan1447 <ahsanchaudary1447@gmail.com>
2025-05-28 15:07:44 +0500

    Initial commit for plugin changes

---
+++ plugin.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+# name: salla_serializers
+# about: Add Salla serializers
+# version: 1.0
+# authors: Ahsan Afzal
+
+enabled_site_setting :custom_core_enabled
+
+after_initialize do
+  %w[
+    basic_category_serializer_extension
+    post_serializer_extension
+    suggested_topic_serializer_extension
+    topic_list_item_serializer_extension
+  ].each do |file|
+    require_relative "app/serializers/#{file}"
+  end
+
+  require_relative "app/controllers/topics_controller.rb"
+  load File.expand_path("app/config/routes.rb", __dir__)
+
+  require_relative "lib/salla_serializers/controller_extensions"
+  ::ApplicationController.class_eval do
+    include ::SallaSerializers::ControllerExtensions
+  end
+
+  require_relative "lib/salla_serializers/tags_controller_patch"
+  ::TagsController.prepend SallaSerializers::TagsControllerPatch
+
+  require_relative "lib/salla_serializers/middleware_patch"
+  ContentSecurityPolicy::Middleware.prepend SallaSerializers::MiddlewarePatch
+
+  require_relative "lib/salla_serializers/email_interceptor"
+  ActionMailer::Base.register_interceptor(SallaSerializers::EmailInterceptor)
+
+  require_relative "lib/salla_serializers/auth_cookie_patch"
+  ::Auth::DefaultCurrentUserProvider.prepend SallaSerializers::AuthCookiePatch
+end
