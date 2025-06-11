5f970f0781b3028d6d1e188875f5e9ffd7526f22
Ahsan1447 <ahsanchaudary1447@gmail.com>
2025-06-11 14:13:47 +0500

    added api endpoint of listing users without auth

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -2,4 +2,5 @@
 
 Discourse::Application.routes.append do
   post "/t/:id/increment_count" => "salla_serializers/topics#increment_count"
+  get "/public-users" => "public_users#index"
 end
---
+++ app/controllers/public_users_controller.rb
@@ -0,0 +1,23 @@
+class ::PublicUsersController < ::ApplicationController
+    skip_before_action :check_xhr
+    skip_before_action :ensure_logged_in
+  
+    def index
+      page = params[:page].to_i > 0 ? params[:page].to_i : 1
+      per_page = 50
+  
+      users = User
+        .where(active: true)
+        .order(:id)
+        .offset((page - 1) * per_page)
+        .limit(per_page)
+        .pluck(:id, :username, :name, :created_at)
+  
+      render json: {
+        users: users.map { |id, username, name, created_at|
+          { id: id, username: username, name: name, created_at: created_at }
+        },
+        page: page
+      }
+    end
+  end
