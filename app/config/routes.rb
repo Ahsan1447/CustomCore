f47e7be24e67eed991361a133a04f6f85853277f
Ahsan1447 <ahsanchaudary1447@gmail.com>
2025-06-12 10:57:04 +0500

    updated route

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 Discourse::Application.routes.append do
-  scope module: 'custom_core' do
-    post "/t/:id/increment_count" => "topics#increment_count"
-    get "/public_users" => "public_users#index"
-  end
+  get "/public_users" => "salla_serializers/public_users#index"
+  post "/t/:id/increment_count" => "salla_serializers/topics#increment_count"
 end
