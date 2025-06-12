dbaf5cda15fadebbf54252d5a6209c8a187bebf4
Ahsan1447 <ahsanchaudary1447@gmail.com>
2025-06-12 10:37:20 +0500

    changed namespace

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-Discourse::Application.routes.draw do
+Discourse::Application.routes.append do
   scope module: 'custom_core' do
     post "/t/:id/increment_count" => "topics#increment_count"
     get "/public_users" => "public_users#index"
